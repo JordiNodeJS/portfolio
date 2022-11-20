68e3ae358fba5c11bfccf2f9f89770b0e72157b1
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-11-20 21:42:34 +0100

    format form

--- js/form.js
+++ js/form.js
@@ -1,32 +1,37 @@
-const $form = document.getElementById("contact");
-    
-async function handleSubmit(event) {
-	event.preventDefault();
-	const $status = document.getElementById("status");
-	const data = new FormData(event.target);
-	fetch(event.target.action, {
-		method: $form.method,
-		body: data,
-		headers: {
-				'Accept': 'application/json'
-		}
-	}).then(response => {
-		if (response.ok) {
-			$status.classList.toggle("alert-success");
-			$status.innerHTML = "Gracias por contactar conmigo!";
-			$form.reset()
-		} else {
-			response.json().then(data => {
-				if (Object.hasOwn(data, 'errors')) {
-					$status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-				} else {
-					$status.classList.toggle("alert alert-warning");
-					$status.innerHTML = "Oops! There was a problem submitting your form"
-				}
-			})
-		}
-	}).catch(error => {
-		$status.innerHTML = "Oops! There was a problem submitting your form"
-	});
+const $form = document.getElementById('contact')
+
+const handleSubmit = async event => {
+  event.preventDefault()
+  const $status = document.getElementById('status')
+  const data = new FormData(event.target)
+  fetch(event.target.action, {
+    method: $form.method,
+    body: data,
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then(response => {
+      if (response.ok) {
+        $status.classList.toggle('alert-success')
+        $status.innerHTML = 'Gracias por contactar conmigo!'
+        $form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            $status.innerHTML = data['errors']
+              .map(error => error['message'])
+              .join(', ')
+          } else {
+            $status.classList.toggle('alert-warning')
+            $status.innerHTML = 'Oops! There was a problem submitting your form'
+          }
+        })
+      }
+    })
+    .catch(error => {
+      $status.classList.toggle('alert-warning')
+      $status.innerHTML = 'Oops! There was a problem submitting your form'
+    })
 }
-$form.addEventListener("submit", handleSubmit)
+$form.addEventListener('submit', handleSubmit)
