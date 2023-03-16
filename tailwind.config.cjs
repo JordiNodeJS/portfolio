e0254f6809a501105687fb1e3554409cc8b7cec6
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-03-16 14:34:35 +0100

    fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,24 +12,16 @@ module.exports = {
       {
         porfolio: {
           primary: "#FF79C6",
-
           secondary: "#BD93F9",
-
           accent: "#FFB86C",
-
           neutral: "#414558",
-
           "base-100": "#282A36",
-
           info: "#8BE9FD",
-
           success: "#50FA7B",
-
           warning: "#F1FA8C",
-
           error: "#FF5555",
         },
       },
     ],
   },
-};
+}
