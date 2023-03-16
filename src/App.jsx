bc8aabcfd836caafcfc6ba92cf019c2439c55b05
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-03-16 13:51:34 +0100

    --amend

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <div><h1 className='text-lg text-purple-700 font-extrabold'>Hola mundo</h1></div>
-  )
+    <div>
+      <h1 className="text-lg text-purple-700 font-extrabold">Hola mundo</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
