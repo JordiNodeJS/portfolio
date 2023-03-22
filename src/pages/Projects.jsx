b0e374e631c24555616e68160ef1e9cfdb1ccf1e
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-03-22 16:25:10 +0100

    add tags

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,22 @@
 import Container from '../layouts/Container'
+import db from '../../db/db.json'
+const { projects } = db
 
 export default function Projects() {
   return (
     <Container>
       <h1 className="mb-5 text-5xl font-bold">Title</h1>
-      <p className="mb-5 text-left text-neutral-300">description</p>
+      {projects.map((project) => (
+        <ul key={project.id}>
+          <li>{project.title}</li>
+          <li>{project.description}</li>
+          {project.tags?.map((tag) => (
+            <div key={project.id + tag} className="badge badge-info gap-2 mr-2">
+              {tag}
+            </div>
+          ))}
+        </ul>
+      ))}
     </Container>
   )
 }
