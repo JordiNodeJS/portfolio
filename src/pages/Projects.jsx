4227731050cd82d484d0f4ff0606953eb3b05849
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-03-22 16:27:50 +0100

    add title

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,15 +5,13 @@ const { projects } = db
 export default function Projects() {
   return (
     <Container>
-      <h1 className="mb-5 text-5xl font-bold">Title</h1>
+      <h1 className="mb-5 text-5xl font-bold">Projects</h1>
       {projects.map((project) => (
         <ul key={project.id}>
           <li>{project.title}</li>
           <li>{project.description}</li>
           {project.tags?.map((tag) => (
-            <div key={project.id + tag} className="badge badge-info gap-2 mr-2">
-              {tag}
-            </div>
+            <div key={project.id + tag} className="badge badge-info gap-2 mr-2">{tag}</div>
           ))}
         </ul>
       ))}
