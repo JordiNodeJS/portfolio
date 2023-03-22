ae952d9d28d3c943f8e631e3d68d67eb8d5a4c0d
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-03-22 14:20:58 +0100

    refactor home

--- src/components/nav-bar.jsx
+++ src/components/nav-bar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom'
 
 export default function NavBar() {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar hero-overlay">
       <div className="flex-1">
         <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,21 @@
+import db from '../../db/db.json'
 export default function Home() {
+  const {
+    presentation: { name, title, description }
+  } = db
+
   return (
     <div
       className="hero height-min"
-      style={{
-        backgroundImage: 'url("azamat.jpg")'
-      }}
+      // style={{
+      //   backgroundImage: 'url("azamat.jpg")'
+      // }}
     >
-      <div className="hero-overlay bg-opacity-90 background"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h2 className="">FrontEnd React Developer</h2>
-          <h1 className="mb-5 text-5xl font-bold">JORGe&apos;s web</h1>
-          <p className="mb-5 text-left text-neutral-300">
-            + 2 years of experience in the programming world, specializing in
-            React. Committed to developing accessible and high-performance user
-            experiences.
-          </p>
+          <h2 className="mb-2">{title}</h2>
+          <h1 className="mb-5 text-5xl font-bold">{name}</h1>
+          <p className="mb-5 text-left text-neutral-300">{description}</p>
           <button className="btn btn-secondary bg-opacity-40 hover:bg-opacity-70">
             Get Started
           </button>
