3bf5d213f4a561804d3606d330ead4070519c211
JORDI.NODEJS <jordi.nodejs@gmail.com>
2023-03-23 23:59:22 +0100

    add studies

--- src/pages/Studies.jsx
+++ src/pages/Studies.jsx
@@ -1,5 +1,59 @@
+import { useState } from 'react'
 import Container from '../layouts/Container'
 
 export default function Studies() {
-  return <Container>Studies</Container>
+  const [steps, setSteps] = useState(['', '', '', ''])
+
+  const handleStep = (index) => () => {
+    const newSteps = [...steps]
+    newSteps[index] = 'step-secondary'
+    setSteps(newSteps)
+  }
+
+  return (
+    <Container>
+      <ul className="steps steps-vertical">
+        <li
+          data-content="📚"
+          onMouseOver={handleStep(0)}
+          className={`step ${steps[0]} relative`}
+        >
+          <div className="">
+            <div>Register</div>
+            <div>Register</div>
+            <div>Register</div>
+            <div>Register</div>
+            <div>Register</div>
+            <div>Register</div>
+            <div>Register</div>
+            <div>Register</div>
+            <div>Register</div>
+            <div>Register</div>
+            <div>Register</div>
+          </div>
+        </li>
+        <li
+          data-content="📚"
+          onMouseOver={handleStep(1)}
+          className={`step ${steps[1]}`}
+        >
+          Choose plan
+        </li>
+        <li
+          data-content="📚"
+          onMouseOver={handleStep(2)}
+          className={`step ${steps[2]}`}
+        >
+          Purchase
+        </li>
+        <li
+          data-content="📚"
+          onMouseOver={handleStep(3)}
+          className={`step ${steps[3]}`}
+        >
+          Receive Product
+        </li>
+      </ul>
+    </Container>
+  )
 }
